4266d44d1710afa6bb190d8d76690c1688b0592b
eknat <eknat@ASUS>
2026-08-20 12:51:24 +0530

    Initial Selenium project setup

---
+++ selenium-automation/src/test/java/com/example/selenium_automation/DailyAutomationTest.java
@@ -0,0 +1,15 @@
+package com.example.selenium_automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DailyAutomationTest {
+    @Test
+    public void openWebsite() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://example.com");
+        System.out.println("Page Title: " + driver.getTitle());
+        driver.quit();
+    }
+}
