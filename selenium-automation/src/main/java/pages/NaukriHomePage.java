ebbb41e8ed4c0eb893321ff1a7434a0fc92589a7
eknat <eknat@ASUS>
2026-08-22 14:07:39 +0530

    resume added

--- selenium-automation/src/main/java/pages/NaukriHomePage.java
+++ selenium-automation/src/main/java/pages/NaukriHomePage.java
@@ -5,44 +5,52 @@ import org.openqa.selenium.WebDriver;
 
 public class NaukriHomePage {
 
-				WebDriver driver;
-				
-				public NaukriHomePage(WebDriver driver) {
-			        this.driver = driver;
-			    }
+    WebDriver driver;
 
-			    // Locators using XPath
-			    By usernameField = By.xpath("//input[@id='usernameField']");
-			    By passwordField = By.xpath("//input[@id='passwordField']");
-			    By loginButton   = By.xpath("//button[text()='Login']");
-			    By myProfileBtn  = By.xpath("(//div[@class='view-profile-wrapper']/a)[1]");
-			    By deleteResumeBtn  = By.xpath("//span[@data-title='delete-resume']");
-			    
-			    
-		        
-			    // Actions
-			    public void openUrl(String url) {
-			        driver.get(url);
-			    }
-			    
-			    public void enterUsername(String username) {
-			        driver.findElement(usernameField).sendKeys(username);
-			    }
+    public NaukriHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
 
-			    public void enterPassword(String password) {
-			        driver.findElement(passwordField).sendKeys(password);
-			    }
+    // Locators
+    By usernameField     = By.xpath("//input[@id='usernameField']");
+    By passwordField     = By.xpath("//input[@id='passwordField']");
+    By loginButton       = By.xpath("//button[text()='Login']");
+    By myProfileBtn      = By.xpath("(//div[@class='view-profile-wrapper']/a)[1]");
+    By deleteResumeBtn   = By.xpath("//span[@data-title='delete-resume']");
+    By confirmDeleteBtn  = By.xpath("//p[text()='Are you sure you want to delete the resume?']//following::button[text()='Delete']");
+    By resumeUploadBox   = By.xpath("//input[@type='file']"); 
 
-			    public void clickLoginButton() {
-			        driver.findElement(loginButton).click();
-			    }
-			    
-			    public void clickmyProfileBtn() {
-			        driver.findElement(myProfileBtn).click();
-			    }
-			    
-			    public void clickdeleteResumeBtn() {
-			        driver.findElement(deleteResumeBtn).click();
-			    }
+    // Actions
+    public void openUrl(String url) {
+        driver.get(url);
+    }
 
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void clickmyProfileBtn() {
+        driver.findElement(myProfileBtn).click();
+    }
+
+    public void clickdeleteResumeBtn() {
+        driver.findElement(deleteResumeBtn).click();
+    }
+
+    public void clickconfirmDeleteBtn() {
+        driver.findElement(confirmDeleteBtn).click();
+    }
+
+    // ✅ Upload directly via hidden input
+    public void selectResumeFile(String filePath) {
+        driver.findElement(resumeUploadBox).sendKeys(filePath);
+    }
 }
--- selenium-automation/src/test/java/com/example/selenium_automation/NaukariMe.java
+++ selenium-automation/src/test/java/com/example/selenium_automation/NaukariMe.java
@@ -14,42 +14,45 @@ import org.testng.annotations.Test;
 import pages.NaukriHomePage;
 
 public class NaukariMe {
-	
-	WebDriver driver;
-	NaukriHomePage NaukriHomePage;
-	
-	@BeforeMethod
-    	public void setUp() {
-        	driver = new ChromeDriver();
-        	// Initialize page object once at class level
-        	NaukriHomePage = new NaukriHomePage(driver);
+    
+    WebDriver driver;
+    NaukriHomePage NaukriHomePage;
+    
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        NaukriHomePage = new NaukriHomePage(driver);
     }
-	
-	 @Test
-	    public void openWebsite() throws IOException {
-		 Properties prop = new Properties();
-	        FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
-			prop.load(fis);
-		
-	        String url = prop.getProperty("naukariurl");
-	        String user = prop.getProperty("username");
-	        String pass = prop.getProperty("password");
-	        
-	        WebDriver driver = new ChromeDriver();
-	        driver.manage().window().maximize();
-	     	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	        
-	     	NaukriHomePage.openUrl(url);
-	     	NaukriHomePage.enterUsername(user);
-	     	NaukriHomePage.enterPassword(pass);
-	     	NaukriHomePage.clickdeleteResumeBtn();
-	     	
-	        System.out.println("Page Title: " + driver.getTitle());
-	        driver.quit();
-	    }
-	 
-	 @AfterMethod
-	    public void tearDown() {
-	        driver.quit();
-	    }
+    
+    @Test
+    public void openWebsite() throws IOException, InterruptedException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+        prop.load(fis);
+
+        String url = prop.getProperty("naukariurl");
+        String user = prop.getProperty("username");
+        String pass = prop.getProperty("password");
+        String filepath = System.getProperty("user.dir") + "/src/test/resources/Eknath_Ugile_Resume.pdf";
+
+        NaukriHomePage.openUrl(url);
+        NaukriHomePage.enterUsername(user);
+        NaukriHomePage.enterPassword(pass);
+        NaukriHomePage.clickLoginButton();
+        NaukriHomePage.clickmyProfileBtn();
+        NaukriHomePage.clickdeleteResumeBtn();
+        NaukriHomePage.clickconfirmDeleteBtn();
+
+        // ✅ Upload directly (no popup)
+        NaukriHomePage.selectResumeFile(filepath);
+
+        Thread.sleep(2000); // optional pause
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
